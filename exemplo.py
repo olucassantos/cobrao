3ac4441ebb6b61dc55fd50b521cc4da3426a7379
Lucas Anjos <oargus.g@gmail.com>
2023-04-22 09:17:57 -0300

    first commit

---
+++ exemplo.py
@@ -0,0 +1,17 @@
+print('Hello World')
+
+# Comentário
+nome = "Lucas"
+_nome = "Lucas"
+
+Nome = input("Digite seu nome: ")
+
+if Nome == "Lucas":
+    print("Olá, Lucas")
+elif Nome == "Leo":
+    print ("Olá, Leo")
+
+    if True:
+        print("Olá, Leo")
+else:
+    print("Olá, desconhecido")
